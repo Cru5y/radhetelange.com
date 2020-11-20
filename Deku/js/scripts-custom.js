937e851ea6ab5dc2c27e06acf442d61f395206af
Cru5y <radhesyam@live.it>
2020-11-20 13:12:19 +0100

    deku

---
+++ Deku/js/scripts-custom.js
@@ -0,0 +1,19 @@
+function sendAnimation() {
+  var element = document.getElementById("sendAnim");
+  element.classList.toggle("panel_force_hover");
+}
+
+function bounceButton() {
+  var element = document.getElementById("sendAnim");
+  element.classList.toggle("panel_bounce");
+
+  setTimeout(function bounceButton() {
+    var element = document.getElementById("sendAnim");
+    element.classList.toggle("panel_bounce");
+  }, (1*300)); //0.5 seconds
+}
+
+function allMight() {
+  var element = document.getElementById("allMightBtn");
+  element.classList.toggle("all-might-button-hover");
+}
